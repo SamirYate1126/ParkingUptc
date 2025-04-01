3abd133bcd70a03f3e7d84e3d99fe1723a9ed039
SamirYate1126 <samir.yate@uptc.edu.co>
2025-04-01 17:14:05 -0500

    Descripción de los cambios realizados

--- src/runner/App.java
+++ src/runner/App.java
@@ -1,6 +1,6 @@
 package runner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, peoplee!");
+        System.out.println("Hello, peoplee 22!");
     }
 }
